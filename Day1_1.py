3d4dc482543916032ad6275814aefc78adcc157b
Manh Quan <manhquan.v33@gmail.com>
2022-10-11 23:13:59 +0700

    First uploading - Update Input Output

---
+++ Day1_1.py
@@ -0,0 +1,9 @@
+my_name = input("What your name ?:\n")
+my_hometown = input("What your hometown ?:\n")
+
+if my_name != "" and my_hometown != "":
+    print("Hello " + my_name + " from "+ my_hometown)
+if my_name == "":
+    print("Please keyin your name") 
+if my_hometown == "":
+    print("Please keyin your hometown") 
