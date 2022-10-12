d84e9b15ccc0a44009220e71ac38fba9fcfb3980
Manh Quan <manhquan.v33@gmail.com>
2022-10-13 00:27:36 +0700

    Update comment

--- Day2_2_BMI.py
+++ Day2_2_BMI.py
@@ -1,10 +1,15 @@
+# Get input from user
 num_height = float(input("Please key-in your height: \n"))
 num_weight = float(input("Please key-in your weight: \n"))
 
+# Calculate processing
 bmi_value = num_weight/num_height**2
+# Using "format" method 
 bmi_two_dec_format = "{:.2f}".format(bmi_value)
+# Using rounf function
 bmi_two_dec_round  = round(bmi_value,3)
 
+# Print result
 print(type(bmi_two_dec_format))
 print(type(bmi_two_dec_round))
 print("Your BMI is: " + bmi_two_dec_format + " (format method)")
