26fc533524902aa919930331d6841e9e0aa20b08
Manh Quan <manhquan.v33@gmail.com>
2022-10-12 23:40:47 +0700

    Adding round function

--- Day2_2_BMI.py
+++ Day2_2_BMI.py
@@ -2,6 +2,11 @@ num_height = float(input("Please key-in your height: \n"))
 num_weight = float(input("Please key-in your weight: \n"))
 
 bmi_value = num_weight/num_height**2
-bmi_two_decimal = "{:.2f}".format(bmi_value)
+bmi_two_dec_format = "{:.2f}".format(bmi_value)
+bmi_two_dec_round  = round(bmi_value,3)
 
-print("Your BMI is: " + bmi_two_decimal)
+print(type(bmi_two_dec_format))
+print(type(bmi_two_dec_round))
+
+print("Your BMI is: " + bmi_two_dec_format + " (format method)")
+print("Your BMI is: " + str(bmi_two_dec_round) + " (round function)")
