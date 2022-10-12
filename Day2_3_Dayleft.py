590096f12dafb7181dba44a9f21bac5604076991
Manh Quan <manhquan.v33@gmail.com>
2022-10-13 00:27:48 +0700

    Update comment

--- Day2_3_Dayleft.py
+++ Day2_3_Dayleft.py
@@ -1,7 +1,10 @@
+# Get input from user
 age = input("What your current age? \n")
 
+# Calculate processing
 days_left = (90 - int(age)) * 365
 weeks_left = (90 - int(age)) * 52
 month_left = (90 - int(age)) * 12
 
+# Print result
 print(f"You have {days_left} days, {weeks_left} weeks, {month_left} months if you live until 90 year old")
