cc89750df9dfa9407bfd885670bf53f8a77b77f8
Manh Quan <manhquan.v33@gmail.com>
2022-10-16 13:35:52 +0700

    Fix all bugs, add comment

--- Day3_Pizza_Calculate.py
+++ Day3_Pizza_Calculate.py
@@ -1,42 +1,60 @@
 print("Welcome to Manh Quan Pizza's Company")
+
+# Get input of Pizza size
 size = input("Which size you want ? S, M or L ? ")
 money = 0
+
+# If Pizza size is S
 if size == "S" or "s":
     money = money + 15
-
-    if input("Did you want Pepperoni ? Y or N ") == "Y":
-        money = money + 2
-    elif input("Did you want Pepperoni ? Y or N ") == "N":
-        # Do nothing
-        pass    
-    else:
-        # Do nothing
-        pass
-
-    if input("Did you want more cheese ? Y or N ") == "Y":
-        money = money + 1
-    elif input("Did you want more cheese ? Y or N ") == "Y":
-        # Do nothing
-        pass
-    else:
-        print("Wrong option extra cheese!!")
-
-if size == "M" or size == "L" or size == "m" or size == "l":
-    money = money + 15
-
-    if input("Did you want Pepperoni ? Y or N ") == "Y" or "y":
-        money = money + 2
-    elif input("Did you want Pepperoni ? Y or N ") == "N" or "n":
-        # Do nothing
-        pass    
-    else:
-        # Do nothing
-        pass
+    print("+15$ for size S")
     
-    if input("Did you want more cheese ? Y or N ") == "Y":
+    # Get input option for pepperoni
+    option_pep = input("Did you want pepperoni ? Y or N ")
+    if option_pep == "Y" or "y":
+        money = money + 2
+        print("+2$ for pepperoni")
+    elif option_pep == "N" or "n":
+        print("+0$ for pepperoni")  
+    else:
+        print("Wrong option pepperoni!!")
+
+    # Get input option for extra cheese
+    option_che = input("Did you want more cheese ? Y or N ")
+    if option_che == "Y" or "y":
         money = money + 1
-    elif input("Did you want more cheese ? Y or N ") == "Y":
-        # Do nothing
-        pass
+        print("+1$ for cheese")
+    elif option_che == "N" or "n":
+        print("+0$ for cheese")
     else:
         print("Wrong option extra cheese!!")
+
+# If Pizza size is M or L
+elif size == "M" or size == "L" or size == "m" or size == "l":
+    money = money + 18
+    print("+18$ for size L or M")
+    
+    option_pep = input("Did you want pepperoni ? Y or N ")
+    if option_pep == "Y" or "y":
+        print("+3$ for pepperoni")
+        money = money + 3
+    elif option_pep == "N" or "n":
+        print("+0$ for pepperoni")   
+    else:
+        print("Wrong option pepperoni!!")
+    
+    option_che = input("Did you want more cheese ? Y or N ")
+    if option_che == "Y" or "y":
+        money = money + 1
+        print("+1$ for cheese")
+    elif option_che == "N" or "n":
+        print("+0$ for cheese")
+    else:
+        print("Wrong option extra cheese!!")
+
+# If input not correct
+else: 
+    print("Wrong size of Pizza")
+
+# Print out result
+print("You need to pay: " + str(money))
