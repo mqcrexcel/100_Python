d8cff17ae0f847c5f43ab2c8dc6a4d7c4c92ef65
Manh Quan <manhquan.v33@gmail.com>
2022-10-17 00:26:26 +0700

    New porject

---
+++ Day3_Simple_Game.py
@@ -0,0 +1,32 @@
+print(''',------------------------------------------.
+`-----------------------------------------. |
+,-----. ,-----.  ,----. ,-.  ,-.,------.  | | ,-. ,-. ,----. ,-----.  ,----.
+`----. |`----. |`----. |`--` | |`------'  `-' `-' | |'----. |`----. |'------`
+,-.  | |,----' |,-.  | |,---.| |,------.  ,-. ,-. | |,----' |,----' |.-----.
+| |  | || ,-. < | |  | || |\ ` || ,----'  | | | | | || ,--. || ,-. <  `---. |
+| `--' || |  | || `--' || | \  || `----.  | `-' `-' || |  | || |  | |.----' |
+`-----' `-'  `-' `----' `-'  | |`------'  `----^----'`-'  `-'`-'  `-' `----'
+                             | `--------------------------------------------.
+                              `---------------------------------------------' ''')
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+step = input("Left or right? ")
+
+if step == "Left":
+    action = input("Swim or wait? ")
+    if action == "wait":
+        door = input("Which doors? red, blue or yellow? ")
+        if door == "red":
+            print("Burned by fire. Game Over.")
+        elif door == "blue":
+            print("Eaten by beasts.. Game Over.")
+        elif door == "yellow":
+            print("You Win!")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by trout. Game Over.")        
+else:
+    print("Fall into a hole. Game Over.")                     
